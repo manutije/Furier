338944c70b4309309e5a8543be79966e7e3a17a1
manutije <manu.tijerina@gmail.com>
2022-02-02 00:35:53 -0600

    First commit

---
+++ generator.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def generate_sine_wave(freq, sample_rate, duration):
+    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    frequencies = x * freq
+    # 2pi because np.sin takes radians
+    y = np.sin((2 * np.pi) * frequencies)
+    return x, y
---
+++ main.py
@@ -0,0 +1,53 @@
+# Imports
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.io.wavfile import write
+from scipy.fft import rfft, rfftfreq
+from scipy.fft import irfft
+from generator import generate_sine_wave
+
+#Start Variables
+SAMPLE_RATE = 44100  # Hertz
+DURATION = 5  # Seconds
+
+_, nice_tone = generate_sine_wave(400, SAMPLE_RATE, DURATION)
+_, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
+noise_tone = noise_tone * 0.3
+
+mixed_tone = nice_tone + noise_tone
+
+normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
+
+# Remember SAMPLE_RATE = 44100 Hz is our playback rate
+write("mysinewave.wav", SAMPLE_RATE, normalized_tone)
+
+plt.plot(normalized_tone[:1000])
+plt.show()
+
+# Number of samples in normalized_tone
+N = SAMPLE_RATE * DURATION
+
+yf = rfft(normalized_tone)
+xf = rfftfreq(N, 1 / SAMPLE_RATE)
+
+plt.plot(xf, np.abs(yf))
+plt.show()
+
+# The maximum frequency is half the sample rate
+points_per_freq = len(xf) / (SAMPLE_RATE / 2)
+
+# Our target frequency is 4000 Hz
+target_idx = int(points_per_freq * 4000)
+
+yf[target_idx - 1 : target_idx + 2] = 0
+
+plt.plot(xf, np.abs(yf))
+plt.show()
+
+new_sig = irfft(yf)
+
+plt.plot(new_sig[:1000])
+plt.show()
+
+norm_new_sig = np.int16(new_sig * (32767 / new_sig.max()))
+write("clean.wav", SAMPLE_RATE, norm_new_sig)
